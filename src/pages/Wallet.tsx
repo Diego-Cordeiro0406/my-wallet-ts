0d83bb4e0b615702b9f094e2f34bb6da26f54304
Diego <diegocordeiro121@gmail.com>
2024-07-30 12:03:29 -0300

    styles: finalizando estilização

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -5,6 +5,7 @@ import { Expense } from "../types/types"
 
 import edit_icon from "../assets/edit_icon.png"
 import delete_icon from "../assets/delete_icon.png"
+import Swal from "sweetalert2"
 
 export default function Wallet() {
   const [expensesData, setExpenses] = useState<Expense[]>([]);
@@ -22,6 +23,13 @@ export default function Wallet() {
     const updatedExpenses = [...expensesData, newExpense];
     setExpenses(updatedExpenses);
     localStorage.setItem('expenses', JSON.stringify(updatedExpenses));
+    Swal.fire({
+      position: "top",
+      icon: "success",
+      title: "Sua despesa foi salva",
+      showConfirmButton: false,
+      timer: 1500
+    });
   };
 
   const removeExpense = (id: string) => {
@@ -81,6 +89,9 @@ export default function Wallet() {
       >
         <section
           className="
+            flex
+            items-center
+            justify-center
             w-[68.25rem]
             h-40
             overflow-y-scroll
@@ -89,72 +100,77 @@ export default function Wallet() {
             bottom-12
           "
         >
-            <table
-            className="
-              table-container
-              border-collapse
-              w-[68.25rem]
-              h-40
-            "
-          >
-            <thead>
-              <tr
+          {
+            expensesData.length > 0 ? (
+              <table
                 className="
-                  text-white
-                  text-center
-                  border
-                  border-solid
-                  border-b-white
-                  border-t-transparent
-                  border-l-transparent
-                  border-r-transparent
+                  table-container
+                  border-collapse
+                  w-[68.25rem]
+                  h-40
                 "
               >
-                <th>Descrição</th>
-                <th>Tag</th>
-                <th>Método de pagamento</th>
-                <th>Valor</th>
-                <th>Moeda</th>
-                <th>Editar/Excluir</th>
-              </tr>
-            </thead>
-            <tbody className="text-center text-[#2FC18C]">
-              {expensesData.map((e, index) => (
-              <tr
-                className={`
-                  border
-                  border-solid
-                  ${index === expensesData.length -1 ? 'border-b-transparent' : 'border-b-[#2FC18C]'}
-                  border-t-transparent
-                  border-l-transparent
-                  border-r-transparent
-                `}
-                key={ e.id }
-              >
-                <td className="h-[1.663rem]">{e.description}</td>
-                <td className="h-[1.663rem]">{e.category}</td>
-                <td className="h-[1.663rem]">{e.payment}</td>
-                <td className="h-[1.663rem]">{Number(e.value).toFixed(2)}</td>
-                <td className="h-[1.663rem]">Real</td>
-                <td className="h-[1.663rem]">
-                  <button
-                    className="mr-2"
-                    data-testid="edit-btn"
-                    onClick={() => startEditing(e) }
+                <thead>
+                  <tr
+                    className="
+                      text-white
+                      text-center
+                      border
+                      border-solid
+                      border-b-white
+                      border-t-transparent
+                      border-l-transparent
+                      border-r-transparent
+                    "
                   >
-                    <img className="size-6" src={edit_icon} alt="edit icon" />
-                  </button>
-                  <button
-                    data-testid="delete-btn"
-                    onClick={() => removeExpense(e.id) }
+                    <th>Descrição</th>
+                    <th>Tag</th>
+                    <th>Método de pagamento</th>
+                    <th>Valor</th>
+                    <th>Moeda</th>
+                    <th>Editar/Excluir</th>
+                  </tr>
+                </thead>
+                <tbody className="text-center text-[#2FC18C]">
+                  {expensesData.map((e, index) => (
+                  <tr
+                    className={`
+                      border
+                      border-solid
+                      ${index === expensesData.length -1 ? 'border-b-transparent' : 'border-b-[#2FC18C]'}
+                      border-t-transparent
+                      border-l-transparent
+                      border-r-transparent
+                    `}
+                    key={ e.id }
                   >
-                    <img className="size-6" src={delete_icon} alt="delete icon" />
-                  </button>
-                </td>
-              </tr>
-            ))}
-            </tbody>
-          </table>
+                    <td className="h-[1.663rem]">{e.description}</td>
+                    <td className="h-[1.663rem]">{e.category}</td>
+                    <td className="h-[1.663rem]">{e.payment}</td>
+                    <td className="h-[1.663rem]">{Number(e.value).toFixed(2)}</td>
+                    <td className="h-[1.663rem]">Real</td>
+                    <td className="h-[1.663rem]">
+                      <button
+                        className="mr-2"
+                        data-testid="edit-btn"
+                        onClick={() => startEditing(e) }
+                      >
+                        <img className="size-6" src={edit_icon} alt="edit icon" />
+                      </button>
+                      <button
+                        data-testid="delete-btn"
+                        onClick={() => removeExpense(e.id) }
+                      >
+                        <img className="size-6" src={delete_icon} alt="delete icon" />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+                </tbody>
+              </table>
+            ) : <h3 className="uppercase font-bold text-[#2FC18C]">Você ainda não cadastrou nenhuma despesa</h3>
+          }
+            
         </section>
         
       </section>
